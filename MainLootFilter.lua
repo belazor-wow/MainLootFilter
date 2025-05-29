4adbc880205d3af3816a45cabaea9afb30e5f160
Belazor <9424286+filliph@users.noreply.github.com>
2025-05-29 23:55:06 +0300

    chore: Add IntelliJ settings / linter cleanup
    - Adds IntelliJ settings for the project
    - Minor cleanup for the linter
    - Fix minor issues in the GitHub issue templates

--- MainLootFilter.lua
+++ MainLootFilter.lua
@@ -5,6 +5,7 @@ local private = select(2, ...) ---@class PrivateNamespace
 ---@param ... any
 ---@return boolean filterMessage Whether to filter this message
 ---@return any ... arg2...arg11
+---@diagnostic disable-next-line: unused-local
 local function MainLootFilter_OnEvent(chatFrame, event, ...)
     if chatFrame:GetName() ~= "ChatFrame1" then
         -- Always allow the message in other chat frames
